291cf63e1053e8c67ca49b7a4401e5cb430cafff
李瑞 <lirui.sep@gmail.com>
2020-10-07 14:05:51 +0800

    ci: CI config

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,7 +11,7 @@ set +x
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=`mvn help:evaluate -Dexpression=project.artifactId | grep "^[^\[]"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
